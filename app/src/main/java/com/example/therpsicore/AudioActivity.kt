12d1d691b2ec7fa34c7fde3d3e898e4b22fc7aaf
Jose Tantera <jose_tantera@yahoo.com.ar>
2020-10-31 21:40:51 -0300

    Andando cambio de actividad

---
+++ app/src/main/java/com/example/therpsicore/AudioActivity.kt
@@ -0,0 +1,11 @@
+package com.example.therpsicore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AudioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_audio)
+    }
+}
--- app/src/main/java/com/example/therpsicore/MainActivity.kt
+++ app/src/main/java/com/example/therpsicore/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.therpsicore
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button_test = findViewById<Button>(R.id.button_test)
+
+        button_test.setOnClickListener{
+            val intent = Intent(this, AudioActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
